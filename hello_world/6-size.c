10d962e80d56936f9190d42a14bd2bb1b9efa233
william0863 <granger.w35@gmail.com>
2022-06-07 06:11:47 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,6 +8,10 @@
 
 int main(void)
 {
-printf("~int");
+printf("Size of a char: %ld", sizeof(char));
+printf("Size of an int: %ld", sizeof(int));
+printf("Size of a long int: %ld", sizeof(long int));
+printf("Size of a long long int: %ld", sizeof(long long int));
+printf("Size of a float: %ld", sizeof(float));
 return (0);
 }
