0c6338166ee3810a732fe8725431f359e8fcd46e
william0863 <granger.w35@gmail.com>
2022-06-09 09:12:32 -0700

    Task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,24 +3,24 @@
  * print_sign - Write a function that prints the sign of a number.
  * @n: first operand
  *
- * Return: Always 0 (Success)
+ * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
  */
 int print_sign(int n)
 {
 if (n > 0)
 {
-return (1);
 _putchar ('+');
+return (1);
 }
 else if (n == 0)
 {
-return (0);
 _putchar ('0');
+return (0);
 }
 else
 {
-return (-1);
 _putchar ('-');
+return (-1);
 }
 _putchar('\n');
 }
