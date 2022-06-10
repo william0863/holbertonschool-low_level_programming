bd75059b839a9f060177240e18b4d76ff0014420
william0863 <granger.w35@gmail.com>
2022-06-10 12:14:35 -0700

    Task 8

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * jack_bauer - Write a function that prints every minute of the day  from 00:00 to 23:59.
- * @n: first operand
+ * jack_bauer - print every minute of the day  from 00:00 to 23:59.
+ *
  *
  * Return: Always 0.
  */
