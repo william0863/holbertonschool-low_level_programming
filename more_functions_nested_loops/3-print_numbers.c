1fe284e36d589bcfb94ff364f9e64bde7a65519e
william0863 <granger.w35@gmail.com>
2022-06-13 03:36:51 -0700

    Task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,10 @@
 
 /**
  * print_numbers - A function that checks for a digit (0 through 9).
- * @c : operand
- * 
+ *
+ *
  */
 void print_numbers(void)
-
 {
 char c;
 for (c = '0'; c <= '9'; c++)
