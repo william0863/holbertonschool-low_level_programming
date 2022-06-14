ff3a1ecd2704c7ac29be3e9e6198a179bceff0b7
william0863 <granger.w35@gmail.com>
2022-06-14 05:06:21 -0700

    Task 7

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -3,14 +3,22 @@
 /**
  * print_diagonal - Print a diagonal n times with _putchar.
  * @n : number of characters to draw.
+ * @i : 
+ * @b : 
  */
 
 void print_diagonal(int n)
 {
 int i;
-
-for
- 
+int b;
+for (i = n; i <= b; ++i)
+{
+int j;
+for (j = n; j <= i; ++j)
+{
 _putchar('\\');
+}
+_putchar('\n');
+}
 _putchar('\n');
 }
