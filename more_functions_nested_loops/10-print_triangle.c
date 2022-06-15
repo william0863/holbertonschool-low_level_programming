a70151c317fa41c414a7ba305e1f4cb4b1674197
william0863 <granger.w35@gmail.com>
2022-06-15 04:50:39 -0700

    Task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - Print a diagonal n times with _putchar.
+ * @size : number of characters of the triangle.
+ */
+
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+
+if (size <= 0)
+_putchar('\n');
+
+
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j < size; j++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
