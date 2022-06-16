14e349944e8ab6fd7bb112ad4e14590e22c8241f
william0863 <granger.w35@gmail.com>
2022-06-16 10:18:34 -0700

    Task 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, followed by a new line, to stdout.
+ * @str: String
+ */
+
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+  
+_putchar ('\n');
+}
