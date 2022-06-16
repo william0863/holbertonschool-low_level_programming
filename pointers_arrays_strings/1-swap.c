c27cf2b61d460e9fc4645e65361de4f87cb57158
william0863 <granger.w35@gmail.com>
2022-06-16 06:57:40 -0700

    Task 1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
-* reset_to_98 - function that takes a pointer to an int
-* as parameter and updates the value it points to 98.
-* @n: pointer
+* swap_int - Write a function that swaps the values of two integers.
+*@a: pointer
+*@b: pointer
 * Return: Always 0.
 */
 
 void swap_int(int *a, int *b)
 {
-  int i;
-  i = *a;
-  *a = *b;
-  *b = i;
+int i;
+i = *a;
+*a = *b;
+*b = i;
 }
