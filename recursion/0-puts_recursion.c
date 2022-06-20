e497f69056af346c6fc10c027c83c5400c755d51
william0863 <granger.w35@gmail.com>
2022-06-20 07:04:05 -0700

    Task 0 test

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,11 +5,11 @@
  * _puts_recursion - function that prints a string, followed by a new line.
  * @s : string.
  */
- 
+
 void _puts_recursion(char *s)
 {
-  s = "p"; 
-  _putchar(*s);
-  _putchar('\n');
-  return;
+s = "p";
+_putchar(*s);
+_putchar('\n');
+return;
 }
