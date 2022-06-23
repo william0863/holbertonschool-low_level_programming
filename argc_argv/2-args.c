e59129d872b2b5c78f03f5253d561494b6674824
william0863 <granger.w35@gmail.com>
2022-06-23 20:56:15 +0200

    Task 2 fix betty

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,17 @@
-#include<stdio.h>
-#include "main.h"
+/**
+ * main - program that prints all arguments it receives.
+ * @args: manipulate and output command arguments.
+ * @argv: The vector of arguments.
+ * Return: 0.
+ */
 
-    int main(int args,char *argv[])
-    {
-        int i=0;
-        for(i=0;i<args;i++)
-             printf("%s\n",argv[i]);
-        return 0;
-    }
+#include<stdio.h>
+
+int main(int args, char *argv[])
+{
+int i = 0;
+	for (i = 0; i < args; i++)
+		printf("%s\n", argv[i]);
+return (0);
+}
 
