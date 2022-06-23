8db46b898d84b284736df9d0546d4f0d86274340
william0863 <granger.w35@gmail.com>
2022-06-23 20:50:25 +0200

    Task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
+#include "main.h"
 
     int main(int args,char *argv[])
     {
         int i=0;
         for(i=0;i<args;i++)
              printf("\n%s",argv[i]);
+			 printf("\n");
         return 0;
     }
 
