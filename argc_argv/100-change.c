23c45364610e0217b7b4395ddd12c27aa3a68028
william0863 <granger.w35@gmail.com>
2022-06-23 21:08:39 +0200

    adding files

---
+++ argc_argv/100-change.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the minimum number of coins to make change for money.
+ * @argc: the counter of the arguments
+ * @argv: the
+ * Return: 0 on success or 1 if there is less or more than 3arguments
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+}
