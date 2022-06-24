d74a2dfbc85fc1b0e83e04fca3569430bb25d8fe
william0863 <granger.w35@gmail.com>
2022-06-24 16:34:47 +0200

    Task 0 Project 2

--- static_libraries/3-puts.c
+++ static_libraries/3-puts.c
@@ -3,5 +3,5 @@
 void _puts(char *s)
 
 {
-	return (0);
+
 }
--- static_libraries/main.c
+++ static_libraries/main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "libmy.a"
+
 int main(void)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
