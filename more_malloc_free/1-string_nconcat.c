bf500052d6390703162310d682e9543089793f2b
william0863 <granger.w35@gmail.com>
2022-06-29 13:55:16 +0200

    Task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -4,6 +4,7 @@
  * string_nconcat - A function that concatenates two strings.
  * @s1: First string
  * @s2: Second string
+ * @n: length
  * Return: The 2 strings concatened.
  */
 
@@ -11,7 +12,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
 char *concat;
-unsigned int len = n , i;
+unsigned int len = n, i;
 
 if (s1 == NULL)
 s1 = "";
@@ -24,19 +25,21 @@ len++;
 
 for (i = 0; s2[i]; i++)
 len++;
+
 concat = malloc(sizeof(char) * (len));
 
 if (concat == NULL)
 return (NULL);
 
 len = 0;
+
 for (i = 0; s1[i]; i++)
-concat(len++) = s1[i];
+concat[len++] = s1[i];
 
 for (i = 0; s2[i] && i < n; i++)
-concat(len++) = s2[i];
+concat[len++] = s2[i];
 
 concat[len] = '\0';
 
-return(concat);
+return (concat);
 }
