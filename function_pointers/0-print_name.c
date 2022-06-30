37e0ac1a923e4afb41a07a2fca59686cd3339468
william0863 <granger.w35@gmail.com>
2022-06-30 17:21:05 +0200

    Adding files & Task 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - function that prints a name.
+ * @name: name
+ *
+ * Return: nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
