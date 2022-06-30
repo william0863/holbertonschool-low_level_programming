63a677d6f59e92940005e9c17d141db081a6f2b4
william0863 <granger.w35@gmail.com>
2022-06-30 17:24:06 +0200

    Task 0 fix betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * print_name_as_is - function that prints a name.
+ * print_name - function that prints a name.
  * @name: name
  *@f: ptr
  * Return: nothing.
