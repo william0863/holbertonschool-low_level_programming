5669580c12bbd5fdd933de1db295f2d910724865
william0863 <granger.w35@gmail.com>
2022-07-19 20:12:22 +0200

    Task 0 & 1

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,37 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: pointer to a string of 0 and 1 chars.
- *
- * Return: If b is NULL or contains chars not 0 or 1 - 0.
- *  or the converted num
+ * @b: binary
+ * Return: the unsigned int
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
 
-	if (b == '\0')
-		return (0);
+	int len = 0, i;
+	unsigned int sum = 0;
 
-	for (len = 0; b[len];)
+	if (b == NULL)
+		return (sum);
+
+	/* trouver la longueur de la string */
+	while (b[len] != '\0')
 		len++;
+	len -= 1;
 
-	for (len -= 1; len >= 0; len--)
+	/* répéter la string et si atteint 1 multiplier puissance 2 */
+	i = 0;
+	while (b[i])
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+		if ((b[i] != '0') && (b[i] != '1'))
+			return (sum);
 
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		if (b[i] == '1')
+			sum += (1 * (1 << len));
+		i++;
+		len--;
 	}
 
-	return (num);
+	return (sum);
 }
